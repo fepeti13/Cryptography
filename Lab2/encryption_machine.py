f6d98b1811870febd1e0601f5e6af4db1b199a23
fepeti13 <fepeti13@gmail.com>
2025-11-08 22:37:10 +0200

    padding_implemented

--- Lab2/encryption_machine.py
+++ Lab2/encryption_machine.py
@@ -20,16 +20,20 @@ def encryption_machine(original_text, key, size, encryption_function, method, in
         CTR(original_text, key, size, encryption_function, padding_mode)
 
     
-def ECB(original_text, key, size, encryption_function, padding_mode):
+def ECB(original_text, key, encryption_function, padding_mode):
     
     length = len(original_text)
     start_iterator = 0
     end_iterator = BLOCK_SIZE
+    encrypted_text = bytearray()
     while (end_iterator < length):
-        encrypted_text += encryption_function(original_text[start_iterator, end_iterator], key)
+        encrypted_text += encryption_function(original_text[start_iterator: end_iterator], key)
+        start_iterator += BLOCK_SIZE
+        end_iterator += BLOCK_SIZE
 
-    padded_block = padding_function(original_text[start_iterator, length], padding_mode)
-
+    if start_iterator < length:
+        padded_block = padding_function(original_text[start_iterator: length], padding_mode)
+        encrypted_text += encryption_function(padded_block, key)
     pass
 
 def CBC(size, encryption_function, initial_vector, padding_mode):
@@ -44,25 +48,29 @@ def OFB(size, encryption_function, initial_vector, padding_mode):
 def CTR(size, encryption_function, padding_mode):
     pass
 
-def padding_fucntion(text, padding_mode):
+def padding_function(text, padding_mode):
     if padding_mode == "zero-padding":
         return zero_padding(text)
     elif padding_mode == "DES_padding":
         return DES_padding(text)
     elif padding_mode == "Schneier_Ferguson_padding":
-        return "Scheneier_Ferguson_padding"
+        return Schneier_Ferguson_padding(text)
     else:
         print("Incorrect padding mode")
         return -1
     
 def zero_padding(text):
-    length = len(text)
-    nr_zeros = BLOCK_SIZE - length
-    zeros = ""
-    for i in range(nr_zeros):
-        zeros += "0"
+    padding_length = BLOCK_SIZE - len(text)                     #len gives the lenght in bytes
+    return text.extend(b'\x00' * padding_length)
     
-    return text + zeros
+def DES_padding(text):
+    padding_length = BLOCK_SIZE - len(text)
+    return text.extend(b'\x80' + b'\x00' * (padding_length - 1))
+
+def Schneier_Ferguson_padding(text):
+    padding_length = (BLOCK_SIZE - len(text))
+    text.extend(bytes([padding_length] * padding_length))
+    return text
 
 def parse_json(path, filename):
     pass
