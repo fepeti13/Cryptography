16006b74b845c3c32dd2c567ee513f5fc32aa99b
fepeti13 <fepeti13@gmail.com>
2026-01-11 22:09:26 +0200

    cert prints added

--- Lab4/src/main/java/edu/bbte/BNRClient.java
+++ Lab4/src/main/java/edu/bbte/BNRClient.java
@@ -2,34 +2,65 @@ package edu.bbte;
 
 import javax.net.ssl.*;
 import java.io.*;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Date;
 
 public class BNRClient {
     public static void main(String[] args) {
         try {
-            SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() { return null; }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                    }
+            };
 
-            System.out.println("Kapcsolódás a bnr.ro szerverhez...");
-            SSLSocket socket = (SSLSocket) factory.createSocket("bnr.ro", 443);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
 
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            URL url = new URL("https://bnr.ro/");
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
 
-            System.out.println("HTTP GET kérés küldése...");
-            out.println("GET /Home.aspx HTTP/1.1");
-            out.println("Host: bnr.ro");
-            out.println("Connection: close");
-            out.println();
-            out.flush();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36");
+            connection.setRequestProperty("Accept", "text/html");
+            connection.connect();
 
-            System.out.println("\nVálasz:");
+            Certificate[] certs = connection.getServerCertificates();
+
+            for (int i = 0; i < certs.length; i++) {
+                X509Certificate cert = (X509Certificate) certs[i];
+
+                System.out.println("Certificate " + (i + 1));
+                System.out.println("Version: " + cert.getVersion());
+                System.out.println("Serial: " + cert.getSerialNumber().toString(16));
+                System.out.println("Issuer: " + cert.getIssuerX500Principal().getName());
+                System.out.println("Valid from: " + cert.getNotBefore());
+                System.out.println("Valid until: " + cert.getNotAfter());
+                System.out.println("Subject: " + cert.getSubjectX500Principal().getName());
+                System.out.println("Public key algorithm: " + cert.getPublicKey().getAlgorithm());
+                System.out.println("Signature algorithm: " + cert.getSigAlgName());
+
+                String pubKey = java.util.Base64.getEncoder().encodeToString(cert.getPublicKey().getEncoded());
+                System.out.println("Public key: " + pubKey.substring(0, 80) + "...");
+
+                System.out.println();
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
             int count = 0;
-            while ((line = in.readLine()) != null && count++ < 20) {
+            while ((line = in.readLine()) != null && count++ < 15) {
                 System.out.println(line);
             }
 
-            socket.close();
-            System.out.println("\nKapcsolat lezárva.");
+            in.close();
+            connection.disconnect();
 
         } catch (Exception e) {
             e.printStackTrace();
